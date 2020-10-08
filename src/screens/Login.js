58dd52b8ef192b5f250d2359009e077f299fa0d6
Alisson Weiss <alissonweiss@id.uff.br>
2020-10-08 19:04:25 -0300

    Adicionando Redux ao projeto

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react'
 import {View, Text, StyleSheet, TouchableOpacity, TextInput} from 'react-native'
-
-import Register from './Register'
+import {connect} from 'react-redux'
+import {login} from '../store/actions/userActions'
 
 class Login extends Component {
     state = {
+        name: 'Temporário',
         email: '',
         senha: ''
     }
 
     login = () => {
+        this.props.onLogin({...this.state})
         this.props.navigation.navigate('Profile')
     }
 
@@ -42,7 +44,6 @@ class Login extends Component {
             </View>
         )
     }
-
 }
 
 const styles = StyleSheet.create ({
@@ -70,4 +71,10 @@ const styles = StyleSheet.create ({
     }
 })
 
-export default Login
+const mapDispatchToProps = dispatch => {
+    return {
+        onLogin: user => dispatch(login(user))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Login)
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react'
 import {StyleSheet, View, Text, TouchableOpacity} from 'react-native'
 import {Gravatar} from 'react-native-gravatar'
 import Header from '../components/Header'
+import {connect} from 'react-redux'
+import {logout} from '../store/actions/userActions'
 
 class Profile extends Component {
     logout = () => {
+        this.props.onLogout()
         this.props.navigation.navigate('Auth')
     }
 
     render() {
-        const options = { email: 'fulanodetal@gmail.com', secure: true}
+        const options = { email: this.props.email, secure: true}
         return (
             <View style={styles.container}>
                 <Header />
                 <Gravatar options={options} style={styles.avatar} />
-                <Text style={styles.nickname}>Alisson</Text>
-                <Text style={styles.email}>alisson.weiss@hotmail.com</Text>
+                <Text style={styles.nickname}>{this.props.name}</Text>
+                <Text style={styles.email}>{this.props.email}</Text>
                 <TouchableOpacity onPress={this.logout} style={styles.btnLogout}>
                     <Text style={styles.btnText}>Sair</Text>
                 </TouchableOpacity>
@@ -55,4 +58,17 @@ const styles = StyleSheet.create({
     }
 })
 
-export default Profile
+const mapStateToProps = ({ user }) => {
+    return {
+        email: user.email,
+        name: user.name
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onLogout: () => dispatch(logout())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile)
